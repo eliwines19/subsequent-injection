99f90d27bf685cf8e7714b2dbffcef2c9f649408
Elijah Wines <wines.elijah@gmail.com>
2022-08-16 22:01:24 -0400

    performing the right operations, just not permanently changing the result

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 stringOfNums = "21462675756"
 
 function subsequent(str) {
-    // split str into array
-    array = str.split("")
     
     // create a loop that compares the next value in the array to the current value
-    for (var i = 0; i < array.length; i++) {
+    for (var i = 0; i < str.length; i++) {
         // define current element and next element
-        var current = parseInt(array[i], 10)
-        var next = parseInt(array[i+1], 10)
+        var current = parseInt(str[i], 10)
+        var next = parseInt(str[i+1], 10)
 
         // find if numbers are even or odd
         var currentNumStatus = isEvenOrOdd(current)
@@ -16,17 +14,16 @@ function subsequent(str) {
 
         // // if both are even, insert * between the two elements
         // // if both are odd, insert - between the two elements
-
         if (!isNaN(next)) {
             if (currentNumStatus == 'even' && nextNumStatus == 'even') {
-                insert(array, i+1, '*')
+                console.log(str.slice(0, i+1) + '*' + str.slice(i+1))
             } else if (currentNumStatus == 'odd' && nextNumStatus == 'odd') {
-                insert(array, i+1, '-')
+                console.log(str.slice(0, i+1) + '*' + str.slice(i+1))
             }
         }
     }
 
-    return array.join("")
+    return str
 
 }
 
@@ -38,8 +35,4 @@ function isEvenOrOdd(num) {
     }
 }
 
-const insert = (arr, index, newItem) => [
-    ...arr.slice(0, index),
-    newItem,
-    ...arr.slice(index)
-]
+console.log(subsequent(stringOfNums))
